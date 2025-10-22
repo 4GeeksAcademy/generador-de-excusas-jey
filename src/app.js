1d45f5c49deefedbfaaf75bfac3e8b884162248b
jeyLopez <lopezgalvisjeanmary@gmail.com>
2025-10-22 19:33:54 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -25,5 +25,5 @@ function getRandom(anyArray){
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  document.body.innerHTML= getRandom(who) + " " + getRandom(action) + " " + getRandom(what)+ " " + getRandom(when)
+  document.querySelector("#excuse").innerHTML= getRandom(who) + " " + getRandom(action) + " " + getRandom(what)+ " " + getRandom(when)
 };
